c06b129a837dc170af19fa0d416f98acc946a3ca
Enea Xharja <19818937+eneax@users.noreply.github.com>
2022-12-11 13:27:03 +0100

    refactor: remove insert function and display result in console

--- scripts/content.js
+++ scripts/content.js
@@ -1,51 +1,11 @@
-const insert = (content) => {
-  // Find Calmly editor input section
-  const elements = document.getElementsByClassName("droid");
-
-  if (elements.length === 0) return;
-
-  const element = elements[0];
-
-  const pToRemove = element.childNodes[0];
-  pToRemove.remove();
-
-  const splitContent = content.split("\n");
-
-  splitContent.forEach((content) => {
-    const p = document.createElement("p");
-
-    if (content === "") {
-      const br = document.createElement("br");
-      p.appendChild(br);
-    } else {
-      p.textContent = content;
-    }
-
-    element.appendChild(p);
-  });
-
-  return true;
-};
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === "inject") {
     const { content } = request;
+    console.log(content);
 
-    const result = insert(content);
-
-    if (!result) {
+    if (!content) {
       sendResponse({ status: "failed" });
     }
-
-    sendResponse({ status: "success" });
-  }
-});
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "inject") {
-    const { content } = request;
-
-    console.log(content);
 
     sendResponse({ status: "success" });
   }
